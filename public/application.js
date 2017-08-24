e4771f4514b2abbf1d05a331fd2b687989be6073
acarlotto <acarlotto@cox.net>
2017-08-23 20:04:05 -0400

    deploy task

---
+++ public/application.js
@@ -0,0 +1,11 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function($) {"use strict";
+
+	$(function () {});
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ })
+]);
